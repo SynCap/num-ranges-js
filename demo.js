4eb77456d0579f0e8568d9801465ef146a6740fa
Constantin <syncap@ya.ru>
2019-06-06 09:25:55 +0800

    new test for ADD

--- demo.js
+++ demo.js
@@ -11,52 +11,57 @@ const r = new Ranges();
 
 console.log('Ranges is ready!');
 
-spoyom(true, 'Add range', '[1 … 4]');
+spoyom(true, 'ADD', '[1 … 4]', '[1, 4]');
 r.add([1, 4]);
 r.print();
 // Should display: [1, 4]
 
-spoyom(true, 'Add range', '[10 … 20]');
+spoyom(true, 'ADD', '[10 … 20]', '[1, 4] [10, 20]');
 r.add([10, 20]);
 r.print();
 // Should display: [1, 4] [10, 20]
 
-spoyom(true, 'Add range', '[10 … 10]');
+spoyom(true, 'ADD', '[10 … 10]', '[1, 4] [10, 20]');
 r.add([10, 10]);
 r.print();
 // Should display: [1, 4] [10, 20]
 
-spoyom(true, 'Add range', '[21 … 21]');
+spoyom(true, 'ADD', '[21 … 21]', '[1, 4] [10, 21]');
 r.add([21, 21]);
 r.print();
 // Should display: [1, 4] [10, 21]
 
-spoyom(true, 'Add range', '[2 … 4]');
+spoyom(true, 'ADD', '[2 … 4]', '[1, 4] [10, 21]');
 r.add([2, 4]);
 r.print();
 // Should display: [1, 4] [10, 21]
 
-spoyom(true, 'Add range', '[3 … 8]');
+spoyom(true, 'ADD', '[3 … 8]', '[1, 8] [10, 21]');
 r.add([3, 8]);
 r.print();
 // Should display: [1, 8] [10, 21]
 
-spoyom(false, 'Remove range', '[10 … 10]');
+spoyom(false, 'REM', '[10 … 10]', '[1, 8] [11, 21]');
 r.remove([10, 10]);
 r.print();
 // Should display: [1, 8] [11, 21]
 
-spoyom(false, 'Remove range', '[10 … 11]');
+spoyom(false, 'REM', '[10 … 11]', '[1, 8] [12, 21]');
 r.remove([10, 11]);
 r.print();
 // Should display: [1, 8] [12, 21]
 
-spoyom(false, 'Remove range', '[15 … 17]');
+spoyom(false, 'REM', '[15 … 17]', '[1, 8] [12, 14] [18, 21]');
 r.remove([15, 17]);
 r.print();
 // Should display: [1, 8] [12, 14] [18, 21]
 
-spoyom(false, 'Remove range', '[3 … 19]');
+spoyom(false, 'REM', '[3 … 19]', '[1, 2] [20, 21]');
 r.remove([3, 19]);
+r.print();
+// Should display: [1, 2] [20, 21]
+
+spoyom(true, 'ADD', '[17 … 1000000000]', '[1, 2] [17, 1000000000]');
+r.remove([17, 1000000000]);
 r.print();
 // Should display: [1, 2] [20, 21]
